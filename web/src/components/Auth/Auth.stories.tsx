84ce94227c903e8f7d2465301afae3db5c65d127
Ryan Quinn <ryanquinn@Ryans-Mac-mini.local>
2024-01-01 18:33:22 -0800

    auth added 001

---
+++ web/src/components/Auth/Auth.stories.tsx
@@ -0,0 +1,26 @@
+// Pass props to your component by passing an `args` object to your story
+//
+// ```tsx
+// export const Primary: Story = {
+//  args: {
+//    propName: propValue
+//  }
+// }
+// ```
+//
+// See https://storybook.js.org/docs/react/writing-stories/args.
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+import Auth from './Auth'
+
+const meta: Meta<typeof Auth> = {
+  component: Auth,
+  tags: ['autodocs'],
+}
+
+export default meta
+
+type Story = StoryObj<typeof Auth>
+
+export const Primary: Story = {}
--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,14 +1,19 @@
 import { Metadata } from '@redwoodjs/web'
 
+import { useAuth } from 'src/auth'
+import Account from 'src/components/Account/Account'
+import Auth from 'src/components/Auth/Auth'
 import Lobby from 'src/components/Lobby/Lobby'
 
 const HomePage = () => {
+  const { isAuthenticated } = useAuth()
   return (
     <>
       <Metadata title="Home" description="Home page" />
 
       <div>
         <Lobby />
+        <>{!isAuthenticated ? <Auth /> : <Account />}</>
       </div>
     </>
   )
