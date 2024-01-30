69fec979ab12b270b12f2a6d9b931ffca8570b32
Ryan Quinn <ryanquinn@Ryans-Mac-mini.local>
2024-01-30 00:57:22 -0800

    added meta tag experiments

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -10,11 +10,13 @@ const HomePage = () => {
   const { isAuthenticated } = useAuth()
   // console.log(supabase.session)
   // const { hasID } = useAuthId()
-  //cats
+  // cats
+  // if you want to redirect to a page
   // https://http.cat/[status_code]
+  // if you want to display error cats
   // https://http.cat/[status_code].jpg
 
-  const codes = [
+  const catCodes = [
     '100',
     '101',
     '102',
@@ -90,14 +92,42 @@ const HomePage = () => {
     '530',
     '599',
   ]
+  const index = Math.floor(Math.random() * catCodes.length)
+  const catImageUrl = ` https://http.cat/${catCodes[index]}.jpg`
+  //render website
+  const website = process.env.PROJECT_WEBSITE
+  //this is your render deployment without the https:// or starter part
+  const shortWebsite = process.env.SHORT_PROJECT_WEBSITE
+  const ogImageUrl =
+    'https://ogcdn.net/6064b869-74ed-4eb9-b76c-0b701ffe7e6b/v4/https%3A%2F%2Fssrtest-web-server.onrender.com/Redwood%20%2B%20Supabase%20%2B%20SSR/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fdocuments%2F2ee09c87-81b7-4f86-8d81-a34b9ac97b04.jpg%3Ftoken%3DwP8l_Zx2mKe-TmSXU9qZITXA4yKbZJhIrlmDsbb86iM%26height%3D225%26width%3D225%26expires%3D33242599958/og.png'
+  const twitterImageUrl =
+    'https://ogcdn.net/6064b869-74ed-4eb9-b76c-0b701ffe7e6b/v4/https%3A%2F%2Fssrtest-web-server.onrender.com/Redwood%20%2B%20Supabase%20%2B%20SSR/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fdocuments%2F2ee09c87-81b7-4f86-8d81-a34b9ac97b04.jpg%3Ftoken%3DwP8l_Zx2mKe-TmSXU9qZITXA4yKbZJhIrlmDsbb86iM%26height%3D225%26width%3D225%26expires%3D33242599958/og.png'
 
-  const random = Math.random() * codes.length
-  const index = Math.floor(random)
-  const imageUrl = ` https://http.cat/${codes[index]}.jpg`
+  const shortContent = 'Redwood + Supabase + SSR'
+  const longContent = 'RedwoodJS + Supabase Auth + Server Side Rendering '
 
   return (
     <>
-      <Metadata title="Home" image={imageUrl} description="Home page" />
+      <Metadata
+        title="Redwood + Supabase + SSR - Home"
+        image={catImageUrl}
+        description="Home page"
+      />
+
+      <Metadata name="description" content={shortContent} />
+
+      <Metadata property="og:url" content={website} />
+      <Metadata property="og:type" content="website" />
+      <Metadata property="og:title" content={shortContent} />
+      <Metadata property="og:description" content={longContent} />
+      <Metadata property="og:image" content={ogImageUrl} />
+
+      <Metadata name="twitter:card" content="summary_large_image" />
+      <Metadata property="twitter:domain" content={shortWebsite} />
+      <Metadata property="twitter:url" content={website} />
+      <Metadata name="twitter:title" content={shortContent} />
+      <Metadata name="twitter:description" content={longContent} />
+      <Metadata name="twitter:image" content={twitterImageUrl} />
 
       <div className="w-full">
         <div className="rw-button font-2xl w-full bg-black px-12 text-center tracking-widest text-white hover:bg-black hover:text-white">
